b4f7dbe8d22bed160fbd845415c6b86333d5d107
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-30 19:22:31 +0300

    Props

--- crash_2022/src/App.vue
+++ crash_2022/src/App.vue
@@ -1,14 +1,18 @@
 <template>
-  <h1>Hello World</h1>
+  <div class="container">
+    <Header title="Task Tracker"/>
+  </div>
 </template>
 
 <script>
-
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
-  components: {}
-}
+  components: {
+    Header
+  }
+};
 </script>
 
 <style>
