7ed67c370c4d63ccb3513b85f2f35d02bca510ca
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-30 20:35:27 +0300

    Single Task Components

--- crash_2022/src/components/Tasks.vue
+++ crash_2022/src/components/Tasks.vue
@@ -1,14 +1,18 @@
-<template>
-    <div v-for="task in tasks" :key="task.id">
-        <h3>{{task.text}}</h3>
-    </div>
-</template>
-
 <script>
+import Task from './Task'
     export default {
         name: 'TasksFile',
         props: {
             tasks: Array
+        },
+        components: {
+            Task
         }
     }
 </script>
+
+<template>
+    <div v-for="task in tasks" :key="task.id">
+        <Task :task="task"/>
+    </div>
+</template>
