5e3385e205d0c994dcd0eab687a6bf8241ab8fe3
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-30 20:08:59 +0300

    Task Tracker

---
+++ crash_2022/src/components/Button.vue
@@ -0,0 +1,13 @@
+<script>
+    export default {
+        name: 'buttonComp',
+        props: {
+            text: String,
+            color: String
+        }
+    }
+</script>
+
+<template>
+    <button :style='{background:color}' class="btn">{{text}}</button>
+</template>
