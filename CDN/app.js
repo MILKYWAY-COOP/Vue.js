4f0dc8f92a786ca48c3373c5e2cd914c567d8a3c
Stephen Muchendu <muchendumaina66@gmail.com>
2022-05-30 18:05:39 +0300

    Working with v-bind

--- CDN/app.js
+++ CDN/app.js
@@ -1,10 +1,14 @@
 const app = Vue.createApp({
-    template: '<h1>Hello {{firstName}}</h1>',
-    data() {
-        return {
-            firstName: 'Stephen',
-        }
-    },
+  data() {
+    return {
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'john@gmail.com',
+      gender: 'Male',
+      image:
+        'https://imgs.search.brave.com/SU0X3VGsDEmbH4IFBJdcGfP3Nvaj67tIFTpN3MLyGYc/rs:fit:504:225:1/g:ce/aHR0cHM6Ly90c2Uz/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5m/VTB1YWYwWldnZlN0/aEFKWFV5bWhnSGFH/OSZwaWQ9QXBp'
+    };
+  }
 });
 
-app.mount('#app')
+app.mount('#app');
